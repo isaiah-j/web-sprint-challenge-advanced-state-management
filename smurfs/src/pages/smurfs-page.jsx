cae48f34ec968cb5886ab529f9ede82cd4c8b188
Isaiah <greatpup1940@gmail.com>
2020-08-14 12:06:35 -0400

    Connected smurfspage to redux store and displaying all smurfs

--- smurfs/src/pages/smurfs-page.jsx
+++ smurfs/src/pages/smurfs-page.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { getAllSmurfs } from '../redux/actions'
 import { connect } from 'react-redux'
-const SmurfsPage = ({ getAllSmurfs }) => {
+import Smurf from '../components/smurf/smurf'
+const SmurfsPage = ({ getAllSmurfs, smurfs }) => {
+    console.log(smurfs)
+
     return (
         <div>
             <h1 onClick={getAllSmurfs}>Smurfs page</h1>
+            {
+                smurfs.map(smurf => {
+                    const {name, age, height, id} = smurf
+                    return <Smurf key={id} age={age} height={height} name={name}></Smurf>
+                })
+            }
         </div>
     )
 }
 
-const mapStateToProps = () => {
-    
+const mapStateToProps = (state) => {
+    return { smurfs: state.smurfs }
 }
 
-export default connect(() => {}, { getAllSmurfs })(SmurfsPage)
+export default connect(mapStateToProps, { getAllSmurfs })(SmurfsPage)
